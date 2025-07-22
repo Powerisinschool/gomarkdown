634d1717164e9084724d1a75a628e820e787898d
Tolulope Olagunju <toolulopeolagunju@gmail.com>
2025-07-22 18:23:24 +0100

    Parse code blocks (indented and fenced)

--- internal/parser/parser.go
+++ internal/parser/parser.go
@@ -17,71 +17,9 @@ func New(input []byte) *Parser {
 
 // Parse is the main entry point for parsing
 func (p *Parser) Parse() *ast.Document {
-	// For now, we'll do the simplest possible thing:
-	// treat the entire input as a single paragraph.
 	doc := ast.NewDocument()
-
-	// TODO: We will want to parse the input into a tree of
-	// ast.InlineNode and ast.BlockNode objects.
-	// TODO: We will want to parse the input line by line, rather than
-	// treating the entire input as a single paragraph.
 	doc.Children = p.parseBlocks()
-	//textNode := &ast.Text{Value: string(p.input)}
-	//paragraphNode := &ast.Paragraph{
-	//	Children: []ast.InlineNode{textNode},
-	//}
-	//doc.Children = []ast.BlockNode{paragraphNode}
-
 	return doc
-}
-
-//func (p *Parser) parseLinesBlock() []ast.BlockNode {
-//	rawBlocks := strings.Split(string(p.input), "\n\n")
-//	blocks := make([]ast.BlockNode, len(rawBlocks))
-//	for i, block := range rawBlocks {
-//		block = strings.TrimSpace(block)
-//		if block == "" {
-//			continue
-//		}
-//		// Parse headings
-//		if block[0] == '#' {
-//			blk := strings.SplitN(block, " ", 2)
-//			level := strings.Count(blk[0], "#")
-//			if level >= 1 && level <= 6 && level == len(blk[0]) {
-//				blocks[i] = &ast.Heading{Level: level, Children: p.inlineParse(blk[1])}
-//				continue
-//			}
-//		}
-//		// Default to a paragraph
-//		if block != "" {
-//			blocks[i] = &ast.Paragraph{Children: p.inlineParse(block)}
-//		}
-//	}
-//	return blocks
-//}
-
-// parseList takes in a slice of lines and a pointer to the current index.
-// It returns a pointer to an ast.List object.
-func (p *Parser) parseList(lines []string, prefix string, i *int) *ast.List {
-	listNode := &ast.List{}
-	for *i < len(lines) {
-		line := lines[*i]
-
-		// If the line doesn't start with the separator, the list is over
-		if !strings.HasPrefix(line, prefix) {
-			break
-		}
-		// Get the content of the list item
-		content := strings.TrimPrefix(line, prefix)
-		// Run the content through the inline parser
-		inlineContent := p.inlineParse(content)
-		// Create the list item and add it to the list
-		listItem := &ast.ListItem{Children: inlineContent}
-		listNode.Items = append(listNode.Items, listItem)
-		// Move to the next line
-		*i++
-	}
-	return listNode
 }
 
 func (p *Parser) parseBlocks() []ast.BlockNode {
@@ -90,15 +28,25 @@ func (p *Parser) parseBlocks() []ast.BlockNode {
 
 	for i := 0; i < len(lines); {
 		line := lines[i]
-		if strings.HasPrefix(line, "    ") {
+		// Check for indented code blocks
+		if strings.HasPrefix(line, "    ") || strings.HasPrefix(line, "\t") {
 			// TODO: Parse the whole code block and update `i`
-			//codeBlockNode := p.parseCodeBlock(lines, &i)
-			//blocks = append(blocks, codeBlockNode)
-			//continue
-			i++
+			prefix := "    "
+			if strings.HasPrefix(line, "\t") {
+				prefix = "\t"
+			}
+			codeBlockNode := p.parseIndentedCodeBlock(lines, prefix, &i)
+			blocks = append(blocks, codeBlockNode)
 			continue
 		}
+		// Remove leading and trailing whitespace from the line
 		line = strings.TrimSpace(line)
+		// Check for fenced code blocks
+		if strings.HasPrefix(line, "```") || strings.HasPrefix(line, "~~~") {
+			codeBlockNode := p.parseFencedCodeBlock(lines, line[:3], &i)
+			blocks = append(blocks, codeBlockNode)
+			continue
+		}
 		// Check for unordered list items
 		if strings.HasPrefix(line, "- ") || strings.HasPrefix(line, "* ") || strings.HasPrefix(line, "+ ") {
 			// Parse the whole list and update `i`
@@ -137,4 +85,69 @@ func (p *Parser) parseBlocks() []ast.BlockNode {
 
 func (p *Parser) inlineParse(line string) []ast.InlineNode {
 	return []ast.InlineNode{&ast.Text{Value: strings.TrimSpace(line)}}
+}
+
+// parseList takes in a slice of lines and a pointer to the current index.
+// It returns a pointer to an ast.List object.
+func (p *Parser) parseList(lines []string, prefix string, i *int) *ast.List {
+	listNode := &ast.List{}
+	for *i < len(lines) {
+		line := lines[*i]
+
+		// If the line doesn't start with the separator, the list is over
+		if !strings.HasPrefix(line, prefix) {
+			break
+		}
+		// Get the content of the list item
+		content := strings.TrimPrefix(line, prefix)
+		// Run the content through the inline parser
+		inlineContent := p.inlineParse(content)
+		// Create the list item and add it to the list
+		listItem := &ast.ListItem{Children: inlineContent}
+		listNode.Items = append(listNode.Items, listItem)
+		// Move to the next line
+		*i++
+	}
+	return listNode
+}
+
+// parseFencedCodeBlock takes in a slice of lines and a pointer to the current index.
+// It returns a pointer to an ast.CodeBlock object.
+func (p *Parser) parseFencedCodeBlock(lines []string, fence string, i *int) *ast.CodeBlock {
+	// Safeguard against invalid input
+	if !strings.HasPrefix(lines[*i], fence) {
+		return &ast.CodeBlock{}
+	}
+	language := strings.TrimSpace(strings.TrimPrefix(lines[*i], fence))
+	*i++
+
+	codeBlock := &ast.CodeBlock{Language: language}
+	for *i < len(lines) {
+		line := lines[*i]
+		if strings.TrimSpace(line) == fence {
+			break
+		}
+		codeBlock.Content += line + "\n"
+		*i++
+	}
+	// Ignore the rest of the content on the closing fence
+	// Kept here in case of further modification as a safeguard
+	// against infinite loops
+	*i++
+	return codeBlock
+}
+
+// parseIndentedCodeBlock takes in a slice of lines and a pointer to the current index.
+// It returns a pointer to an ast.CodeBlock object.
+func (p *Parser) parseIndentedCodeBlock(lines []string, prefix string, i *int) *ast.CodeBlock {
+	codeBlock := &ast.CodeBlock{}
+	for *i < len(lines) {
+		line := lines[*i]
+		if !strings.HasPrefix(line, prefix) {
+			break
+		}
+		codeBlock.Content += strings.TrimPrefix(line, prefix) + "\n"
+		*i++
+	}
+	return codeBlock
 }
